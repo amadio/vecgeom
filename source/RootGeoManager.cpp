6a449199e84631dba60f9ba2ab4f308f3411793e
Guilherme Amadio <amadio@ncc.unesp.br>
2015-09-23 16:55:37 +0200

    Fix comparison of signed/insigned int in RootGeoManager

--- source/RootGeoManager.cpp
+++ source/RootGeoManager.cpp
@@ -121,7 +121,7 @@ VPlacedVolume* RootGeoManager::Convert(TGeoNode const *const node) {
   VPlacedVolume *const placed_volume =
       logical_volume->Place(node->GetName(), transformation);
 
-  unsigned int remaining_daughters = 0;
+  int remaining_daughters = 0;
   {
     // All or no daughters should have been placed already
     remaining_daughters = node->GetNdaughters()
