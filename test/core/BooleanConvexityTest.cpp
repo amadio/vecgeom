321e8dd994003fbaf5609a98e949fb3370f2e0a4
Guilherme Lima <lima@fnal.gov>
2015-11-17 15:39:15 -0600

    [VecGeom-35] Fix compilation warning in Release mode, while also technically avoiding a memory leak

--- test/core/BooleanConvexityTest.cpp
+++ test/core/BooleanConvexityTest.cpp
@@ -8,6 +8,8 @@
 #include "benchmarking/Benchmarker.h"
 //#include "ArgParser.h"
 
+#undef NDEBUG
+#include <cassert>
 
 using namespace vecgeom;
 
@@ -43,6 +45,9 @@ int main(int argc, char * argv[])
     //VPlacedVolume placedBooleanVolume = booleanlogical.Place();
     VPlacedVolume * placedBooleanVolume = (new LogicalVolume("",&booleansolid))->Place();
     assert(!placedBooleanVolume->GetUnplacedVolume()->IsConvex());
+
+    // cleanup
+    delete placedBooleanVolume;
 
     return 0;
 }
