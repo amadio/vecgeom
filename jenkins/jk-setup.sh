ff99e726946006df36550aca47585293600d4173
Oksana Shadura <oksana.shadura@cern.ch>
2015-11-24 10:14:12 +0100

    Testing continuous builds for VecGeom

--- jenkins/jk-setup.sh
+++ jenkins/jk-setup.sh
@@ -18,7 +18,9 @@ else
   return
 fi
 
-if [ $LABEL == slc6 ] || [ $LABEL == cc7 ] || [ $LABEL == cuda7 ] || [ $LABEL == xeonphi ] || [ $LABEL == slc6-physical ]
+if ([ $LABEL == slc6 ] || [ $LABEL == cc7 ] || [ $LABEL == cuda7 ]
+  || [$LABEL == xeonphi ] || [ $LABEL == slc6-physical ]
+  || [  $LABEL == continuous-sl6 ] || [  $LABEL == continuous-cuda7 ] || [ $LABEL == continuous-xeonphi ])
 then
   kinit sftnight@CERN.CH -5 -V -k -t /ec/conf/sftnight.keytab
   export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/3.0.0/Linux-i386/bin:${PATH}
@@ -35,7 +37,7 @@ then
 
   ARCH=$(uname -m)
 
-  if [ $LABEL == cuda7 ] || [ $LABEL == slc6-physical ]
+  if ([ $LABEL == cuda7 ] || [ $LABEL == slc6-physical ] || || [  $LABEL == continuous-sl6 ] || [  $LABEL == continuous-cuda7 ])
   then
     . /afs/cern.ch/sw/lcg/contrib/gcc/${!COMPILERversion}/${ARCH}-slc6/setup.sh
   else
