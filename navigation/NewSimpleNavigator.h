8a88a9586e3cf86c38a4157da893bb81f3a1803c
Sandro Wenzel <sandro.wenzel@cern.ch>
2015-11-25 16:27:21 +0100

    [VecGeom-192] introducing abstract navigators and 3 concrete implementations
    details: a) a new abstract navigator class
             b) 3 concrete implementations of a navigator:
                - "brute force" NewSimpleNavigator
                - SimpleABBoxNavigator based on list of aligned bounding boxes
                - HybridNavigator based on shallow hierachy of lists of aligned bounding boxes;
                  this navigator was integrated from the development of Yang Zhang (technical studentship);
    	      the accompagnying acceleration structure is kept in HybridManager2
    
             c) the design of the navigator is modular; concrete implementations only need to provide few functions
                which are plugged into templated generic code (in particular the hit detection with daughter volumes)
    
             d) navigators may already make use of the convexity property of the mother
    
    todo:  introduce the possibility to use assemblies in navigation; work on the vector interfaces
    
    note: this is a working commit

---
+++ navigation/NewSimpleNavigator.h
@@ -0,0 +1,137 @@
+/*
+ * NewSimpleNavigator.h
+ *
+ *  Created on: 17.09.2015
+ *      Author: swenzel
+ */
+
+#ifndef NAVIGATION_NEWSIMPLENAVIGATOR_H_
+#define NAVIGATION_NEWSIMPLENAVIGATOR_H_
+
+#include "VNavigator.h"
+#include "SimpleSafetyEstimator.h"
+
+namespace vecgeom {
+inline namespace VECGEOM_IMPL_NAMESPACE {
+
+// A very basic implementation of a navigator ( brute force which scales linearly with the number of daughters )
+template <bool MotherIsConvex=false>
+class NewSimpleNavigator : public VNavigatorHelper<class NewSimpleNavigator<MotherIsConvex>, MotherIsConvex> {
+
+private:
+  NewSimpleNavigator() {}
+  virtual ~NewSimpleNavigator() {}
+
+public:
+  VECGEOM_INLINE
+  virtual Precision ComputeStepAndHittingBoundaryForLocalPoint(Vector3D<Precision> const &localpoint,
+                                                               Vector3D<Precision> const &localdir,
+                                                               Precision step_limit, NavigationState const &in_state,
+                                                               NavigationState &out_state) const override {
+
+    auto currentvolume = in_state.Top();
+#if defined(VERBOSE) && !defined(VECGEOM_NVCC)
+    __thread static size_t counter=0;
+    if (counter % 1 == 0) {
+      std::cerr << GetClassName() << " navigating in " << currentvolume->GetLabel() << " stepnumber " << counter << "step_limit" << step_limit
+                << "localpoint " << localpoint << "localdir" << globaldir;
+    }
+    counter++;
+#endif
+
+    Precision step;
+    assert(currentvolume != nullptr && "currentvolume is null in navigation");
+    step = currentvolume->DistanceToOut(localpoint, localdir, step_limit);
+    // NOTE: IF STEP IS NEGATIVE HERE, SOMETHING IS TERRIBLY WRONG. SET TO INFINITY SO THAT WE CAN BETTER HANDLE IT
+    // LATER ON
+    if (step < 0.) {
+      step = kInfinity;
+    }
+
+    // iterate over all daughters
+    auto *daughters = currentvolume->GetLogicalVolume()->GetDaughtersp();
+    decltype(currentvolume) nexthitvolume=nullptr;
+    auto ndaughters = daughters->size();
+    for (decltype(ndaughters) d = 0; d < ndaughters; ++d) {
+      auto daughter = daughters->operator[](d);
+//    previous distance becomes step estimate, distance to daughter returned in workspace
+// SW: this makes the navigation more robust and it appears that I have to
+// put this at the moment since not all shapes respond yet with a negative distance if
+// the point is actually inside the daughter
+#ifdef CHECKCONTAINS
+#pragma message "CHECKCONTAINS"
+      bool contains = daughter->Contains(localpoint);
+      if (!contains) {
+#endif
+        Precision ddistance = daughter->DistanceToIn(localpoint, localdir, step);
+
+        // if distance is negative; we are inside that daughter and should relocate
+        // unless distance is minus infinity
+        bool valid = (ddistance < step && !IsInf(ddistance));
+        nexthitvolume = valid ? daughter : nexthitvolume;
+        step = valid ? ddistance : step;
+#ifdef CHECKCONTAINS
+      } else {
+        std::cerr << " INDA ";
+        step = -1.;
+        nexthitvolume = daughter;
+        break;
+      }
+#endif
+    }
+    return VNavigator::PrepareOutState(in_state, out_state, step, step_limit, nexthitvolume);
+  }
+
+  VECGEOM_INLINE
+  virtual bool
+  CheckDaughterIntersections(LogicalVolume const *lvol, Vector3D<Precision> const & localpoint, Vector3D<Precision> const & localdir,
+                             NavigationState const & in_state, NavigationState & out_state, Precision & step, VPlacedVolume const *&hitcandidate) const override {
+
+    // iterate over all daughters
+    auto *daughters = lvol->GetDaughtersp();
+    auto ndaughters = daughters->size();
+    for (decltype(ndaughters) d = 0; d < ndaughters; ++d) {
+      auto daughter = daughters->operator[](d);
+//    previous distance becomes step estimate, distance to daughter returned in workspace
+// SW: this makes the navigation more robust and it appears that I have to
+// put this at the moment since not all shapes respond yet with a negative distance if
+// the point is actually inside the daughter
+#ifdef CHECKCONTAINS
+#pragma message "CHECKCONTAINS"
+      bool contains = daughter->Contains(localpoint);
+      if (!contains) {
+#endif
+        Precision ddistance = daughter->DistanceToIn(localpoint, localdir, step);
+
+        // if distance is negative; we are inside that daughter and should relocate
+        // unless distance is minus infinity
+        bool valid = (ddistance < step && !IsInf(ddistance));
+        hitcandidate = valid ? daughter : hitcandidate;
+        step = valid ? ddistance : step;
+#ifdef CHECKCONTAINS
+      } else {
+        std::cerr << " INDA ";
+        step = -1.;
+        hitcandidate = daughter;
+        break;
+      }
+#endif
+    }
+  }
+
+
+  static VNavigator *Instance() {
+    static NewSimpleNavigator instance;
+    return &instance;
+  }
+
+  static constexpr const char *gClassNameString = "NewSimpleNavigator";
+  typedef SimpleSafetyEstimator SafetyEstimator_t;
+}; // end of class
+
+}
+} // end namespace
+
+
+
+#endif /* NAVIGATION_NEWSIMPLENAVIGATOR_H_ */
