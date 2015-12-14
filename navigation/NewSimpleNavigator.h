de1e19b1aefd69f39c2f10f9fa5c281e41b7f977
Sandro Wenzel <sandro.wenzel@cern.ch>
2015-12-14 19:55:55 +0100

    introducing SIMD treatment in navigation

--- navigation/NewSimpleNavigator.h
+++ navigation/NewSimpleNavigator.h
@@ -119,6 +119,50 @@ public:
     }
   }
 
+//  template <typename Backend>
+//  VECGEOM_INLINE
+//  virtual Bool_v
+//  CheckDaughterIntersectionsT(LogicalVolume const *lvol, Vector3D<typename Backend::Real_v> const &localpoint,
+//                              Vector3D<typename Backend::Real_v> const &localdir, NavStatePool const &in_state,
+//                              NavStatePool &out_state, unsigned int from_index, unsigned int to_index,
+//                              typename Backend::Real_v &step, VPlacedVolume const **&hitcandidates) const override {
+//
+//    // iterate over all daughters
+//    using Real_v = typename Backend::Real_v;
+//    using Bool_v = Real_v::Mask;
+//    const auto *daughters = lvol->GetDaughtersp();
+//    const auto ndaughters = daughters->size();
+//    for (decltype(ndaughters) d = 0; d < ndaughters; ++d) {
+//      auto daughter = daughters->operator[](d);
+//      // SW: this makes the navigation more robust and it appears that I have to
+//      // put this at the moment since not all shapes respond yet with a negative distance if
+//      // the point is actually inside the daughter
+//      //  #ifdef CHECKCONTAINS
+//      // #pragma message "CHECKCONTAINS"
+//      //        Bool_v contains = daughter->Contains(localpoint);
+//      //        if (!contains) {
+//      //  #endif
+//      Real_v ddistance = daughter->DistanceToIn(localpoint, localdir, step);
+//
+//      // if distance is negative; we are inside that daughter and should relocate
+//      // unless distance is minus infinity
+//      Bool_v valid = (ddistance < step && !IsInf(ddistance));
+//      // serial treatment of hit candidate until I find a better way
+//      for (unsigned int index = valid.firstOne(); index < Real_v::Size; ++index) {
+//        hitcandidates[i] = valid[i] ? daughter : hitcandidates[i];
+//      }
+//      step(valid) = ddistance; // or maskedAssign
+//                               //  #ifdef CHECKCONTAINS
+//                               //        } else {
+//                               //          std::cerr << " INDA ";
+//                               //          step = -1.;
+//                               //          hitcandidate = daughter;
+//                               //          break;
+//                               //        }
+//                               //  #endif
+//    }
+//    return false;
+//  }
 
   static VNavigator *Instance() {
     static NewSimpleNavigator instance;
