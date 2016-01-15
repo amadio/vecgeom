9bced9e3ec765015bb9d96149d688ef1a1ec36a2
Oksana Shadura <oksana.shadura@cern.ch>
2016-01-15 16:26:25 +0100

    Small cleaning

--- jenkins/jk-setup.sh
+++ jenkins/jk-setup.sh
@@ -3,8 +3,6 @@
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-kinit sftnight@CERN.CH -5 -V -k -t /ec/conf/sftnight.keytab
-
 THIS=$(dirname ${BASH_SOURCE[0]})
 
 # first arguments is the source directory
