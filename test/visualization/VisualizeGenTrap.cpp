00814624cbcbc74f5f4b4f31e7f64935dc3cd931
Mihaela Gheata <Mihaela.Gheata@cern.ch>
2016-02-08 17:38:31 +0100

    [VECGEOM-67] More clang-format

--- test/visualization/VisualizeGenTrap.cpp
+++ test/visualization/VisualizeGenTrap.cpp
@@ -8,18 +8,18 @@ using namespace vecgeom;
 
 int main() {
   constexpr int nSamples = 10000;
-  std::vector<Vector3D<Precision> > vertexlist;
+  std::vector<Vector3D<Precision>> vertexlist;
   // no twist
-  vertexlist.push_back( Vector3D<Precision>(-3,-2.5, 0 ) );
-  vertexlist.push_back( Vector3D<Precision>(-2.5, 3, 0 ) );
-  vertexlist.push_back( Vector3D<Precision>( 3, 2.5, 0 ) );
-  vertexlist.push_back( Vector3D<Precision>( 2.5,-3, 0 ) );
-  vertexlist.push_back( Vector3D<Precision>(-2,-2, 0 ) );
-  vertexlist.push_back( Vector3D<Precision>(-2, 2, 0 ) );
-  vertexlist.push_back( Vector3D<Precision>( 2, 2, 0 ) );
-  vertexlist.push_back( Vector3D<Precision>( 2,-2, 0 ) );
+  vertexlist.push_back(Vector3D<Precision>(-3, -2.5, 0));
+  vertexlist.push_back(Vector3D<Precision>(-2.5, 3, 0));
+  vertexlist.push_back(Vector3D<Precision>(3, 2.5, 0));
+  vertexlist.push_back(Vector3D<Precision>(2.5, -3, 0));
+  vertexlist.push_back(Vector3D<Precision>(-2, -2, 0));
+  vertexlist.push_back(Vector3D<Precision>(-2, 2, 0));
+  vertexlist.push_back(Vector3D<Precision>(2, 2, 0));
+  vertexlist.push_back(Vector3D<Precision>(2, -2, 0));
 
-  SimpleGenTrap trap("gentrap", &vertexlist[0], 10 );
+  SimpleGenTrap trap("gentrap", &vertexlist[0], 10);
   TPolyMarker3D pm(nSamples);
   pm.SetMarkerColor(kRed);
   pm.SetMarkerStyle(6);
@@ -28,20 +28,21 @@ int main() {
   for (int i = 0; i < nSamples; ++i) {
     Vector3D<Precision> sample;
     sample = trap.GetPointOnSurface();
-    inside  = trap.Inside(sample);
-    if (inside != kSurface) nerrors++;
-/*
-   bool contains;
-   do {
-      sample = volumeUtilities::SamplePoint(Vector3D<Precision>(4, 4, 10));
-      sample = trap.GetPointOnSurface();
-      inside = trap.Inside(sample);
-      contains = trap.Contains(sample);
-      if (inside==kInside && !contains) {
-        inside = trap.Inside(sample);
-      }
-    } while (inside != kInside);
-*/
+    inside = trap.Inside(sample);
+    if (inside != kSurface)
+      nerrors++;
+    /*
+       bool contains;
+       do {
+          sample = volumeUtilities::SamplePoint(Vector3D<Precision>(4, 4, 10));
+          sample = trap.GetPointOnSurface();
+          inside = trap.Inside(sample);
+          contains = trap.Contains(sample);
+          if (inside==kInside && !contains) {
+            inside = trap.Inside(sample);
+          }
+        } while (inside != kInside);
+    */
     pm.SetNextPoint(sample[0], sample[1], sample[2]);
   }
   Visualizer visualizer;
