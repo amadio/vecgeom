2b383f2ff0310e2471ed850697094cb33268edbc
Oksana Shadura <oksana.shadura@cern.ch>
2016-02-11 12:28:31 +0100

    Update of CMake version used for builds

--- jenkins/jk-setup.sh
+++ jenkins/jk-setup.sh
@@ -19,11 +19,11 @@ fi
 
 if [ $LABEL == slc6 ] || [ $LABEL == cc7 ] || [ $LABEL == cuda7 ] || [ $LABEL == slc6-physical ] || [  $LABEL == continuous-sl6 ] || [  $LABEL == continuous-cuda7 ] || [ $LABEL == continuous-xeonphi ]
 then
-  export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/3.0.0/Linux-i386/bin:${PATH}
+  export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/3.3.2/Linux-x86_64/bin/:${PATH}
   kinit sftnight@CERN.CH -5 -V -k -t /ec/conf/sftnight.keytab
 elif [ $LABEL == xeonphi ]
 then
-  export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/3.0.0/Linux-i386/bin:${PATH}
+  export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/3.3.2/Linux-x86_64/bin:${PATH}
   kinit sftnight@CERN.CH -5 -V -k -t /data/sftnight/ec/conf/sftnight.keytab
 else
   export EXTERNALDIR=$HOME/ROOT-externals/
