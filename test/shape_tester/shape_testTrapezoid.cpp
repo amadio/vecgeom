ddf57382852d3e49095d1ccf5b09f7234c1cb1f7
Guilherme Lima <lima@fnal.gov>
2016-03-14 16:28:54 -0500

    [VECGEOM-226]  Improved source code conventions

--- test/shape_tester/shape_testTrapezoid.cpp
+++ test/shape_tester/shape_testTrapezoid.cpp
@@ -16,39 +16,34 @@ using Trap_t = vecgeom::SimpleTrapezoid;
 int main(int argc,char *argv[]) {
   int errCode= 0;
   VUSolid* solid;
-  if(argc>1){
-    if(strcmp(argv[1],"vec")==0)
-    {
+  if(argc>1) {
+    if(strcmp(argv[1],"vec")==0) {
       solid = new Trap_t("test_VecGeomTrap",5.,0.,0.,5.,5.,5.,0.,5.,5.,5.,0.);   
     }
-    else
-    {   
+    else {   
      solid = new UTrap("test_USolidsTrap",40,0,0,30,20,20,0,30,20,20,0);  
-     solid->StreamInfo(std::cout);      
     }
   }
-  else
-    {
-     solid = new Trap_t("test_VecGeomTrap",5.,0.,0.,5.,5.,5.,0.,5.,5.,5.,0.);    
-    }
+  else {
+    solid = new Trap_t("test_VecGeomTrap",5.,0.,0.,5.,5.,5.,0.,5.,5.,5.,0.);    
+  }
+  solid->StreamInfo(std::cout);      
   
   ShapeTester tester;
 
-  if(argc>2)
-  {
-    if(strcmp(argv[2],"vis")==0)
-    {
-     #ifdef VECGEOM_ROOT
-     TApplication theApp("App",0,0);
-     errCode = tester.Run(solid);
-     theApp.Run();
-     #endif
+  if(argc>2) {
+    if(strcmp(argv[2],"vis")==0) {
+#ifdef VECGEOM_ROOT
+      TApplication theApp("App",0,0);
+      errCode = tester.Run(solid);
+      theApp.Run();
+#endif
     }
   }
-  else
-  {
+  else {
+    //tester.SetMethod("Consistency");
     errCode = tester.Run(solid);
-   }
+  }
   std::cout<<"Final Error count for Shape *** "<<solid->GetName()<<"*** = "<<errCode<<std::endl;
   std::cout<<"========================================================="<<std::endl;
   return 0;
