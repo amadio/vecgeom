f20459cc6d24d5eb205988da970d34d18ee5d5ee
Guilherme Lima <lima@fnal.gov>
2016-04-15 14:41:57 -0500

    [VECGEOM-35] White space and coding conventions

--- test/shape_tester/shape_testBox.cpp
+++ test/shape_tester/shape_testBox.cpp
@@ -13,52 +13,40 @@
 //typedef UBox Box_t;
 typedef vecgeom::SimpleBox Box_t;
 
-int main(  int argc,char *argv[]) {
-
- int errCode= 0;
- VUSolid* box;
-  if(argc>1){
-    if(strcmp(argv[1],"vec")==0)
-    {
+int main( int argc,char *argv[] )
+{
+  int errCode= 0;
+  VUSolid* box;
+  if(argc>1) {
+    if(strcmp(argv[1],"vec")==0) {
      box=new Box_t("test_VecGeomBox",5.,5.,5.);
-     box->StreamInfo(std::cout);
     }
-    else
-    {   
+    else {
      box=new UBox("test_USolidsBox",5.,5.,5.);
-     box->StreamInfo(std::cout);
     }
   }
-  else
-    {
+  else {
      box=new Box_t("test_VecGeomBox",5.,5.,5.);
-     box->StreamInfo(std::cout);
     }
+  box->StreamInfo(std::cout);
+
   ShapeTester tester;
 
-  if(argc>2)
-  {
-    if(strcmp(argv[2],"vis")==0)
-    {
-     #ifdef VECGEOM_ROOT
-     TApplication theApp("App",0,0);
-     errCode = tester.Run(box);
-     theApp.Run();
-     #endif
+  if (argc>2) {
+    if (strcmp(argv[2],"vis")==0) {
+#ifdef VECGEOM_ROOT
+      TApplication theApp("App",0,0);
+      errCode = tester.Run(box);
+      theApp.Run();
+#endif
     }
   }
-  else
-  {
+  else {
+    //tester.SetMethod("Consistency");
+    //tester.SetMethod("XRayProfile");
     errCode = tester.Run(box);
-
-   }
-  std::cout<<"Final Error count for Shape *** "<<box->GetName()<<"*** = "<<errCode<<std::endl;
-  std::cout<<"========================================================="<<std::endl;
+  }
+  std::cout<<"Final Error count for Shape *** "<<box->GetName()<<"*** = "<< errCode <<std::endl;
+  std::cout<<"=========================================================\n";
   return 0;
 }
-
-
-
-
-
-
