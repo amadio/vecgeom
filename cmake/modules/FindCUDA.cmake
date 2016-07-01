c47e212fa4635044114143e302457c342b0233da
Federico Carminati <federico.carminati@cern.ch>
2016-07-01 13:51:05 +0200

    Improve debug message

--- cmake/modules/FindCUDA.cmake
+++ cmake/modules/FindCUDA.cmake
@@ -1702,12 +1702,11 @@ macro(CUDA_ADD_LIBRARY_DEPEND cuda_target) # cuda_depend)
          set(_lib_dependencies ${_lib_dependencies} ${arg} )
       endif()
    endforeach()
-   message("In CUDA_ADD_LIBRARY_DEPEND we have:  ${_lib_dependencies}")
+   message("In CUDA_ADD_LIBRARY_DEPEND@target:${cuda_target} we have:  ${_lib_dependencies}")
    set_property(TARGET ${cuda_target}
                 PROPERTY CUDA_LIBRARY_DEPEND ${_lib_dependencies})
    set_property(TARGET ${cuda_target}
                 PROPERTY CUDA_LIBRARY_DEPEND_TARGET ${_lib_target_dependencies})
-
 endmacro()
 
 ###############################################################################
