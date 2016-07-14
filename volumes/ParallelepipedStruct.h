64d2fc22d0e69acc3b6e9f2ea39fa93b06b35cc3
Mihaela Gheata <Mihaela.Gheata@cern.ch>
2016-07-14 17:09:28 +0200

    [VECGEOM-290] Clang format for the para structure.

--- volumes/ParallelepipedStruct.h
+++ volumes/ParallelepipedStruct.h
@@ -26,44 +26,41 @@ struct ParallelepipedStruct {
   Vector3D<T> fNormals[3]; /** Precomputed normals */
 
   // Precomputed values computed from parameters
-  T fTanAlpha;             /** Tangent of alpha angle */
-  T fTanThetaSinPhi;       /** tan(theta)*sin(phi) */
-  T fTanThetaCosPhi;       /** tan(theta)*cos(phi) */
+  T fTanAlpha;       /** Tangent of alpha angle */
+  T fTanThetaSinPhi; /** tan(theta)*sin(phi) */
+  T fTanThetaCosPhi; /** tan(theta)*cos(phi) */
 
   VECGEOM_CUDA_HEADER_BOTH
-  ParallelepipedStruct(Vector3D<T> const &dim, const T alpha, const T theta, const T phi) 
-    : fDimensions(dim), fAlpha(0), fTheta(0), fPhi(0), fCtx(0), fCty(0), fTanAlpha(0),
-      fTanThetaSinPhi(0), fTanThetaCosPhi(0) {
+  ParallelepipedStruct(Vector3D<T> const &dim, const T alpha, const T theta, const T phi)
+      : fDimensions(dim), fAlpha(0), fTheta(0), fPhi(0), fCtx(0), fCty(0), fTanAlpha(0), fTanThetaSinPhi(0),
+        fTanThetaCosPhi(0)
+  {
     SetAlpha(alpha);
     SetThetaAndPhi(theta, phi);
-  }  
+  }
 
   VECGEOM_CUDA_HEADER_BOTH
   ParallelepipedStruct(const T x, const T y, const T z, const T alpha, const T theta, const T phi)
-    : fDimensions(x, y, z), fAlpha(0), fTheta(0), fPhi(0), fCtx(0), fCty(0), fTanAlpha(0),
-      fTanThetaSinPhi(0), fTanThetaCosPhi(0) {
+      : fDimensions(x, y, z), fAlpha(0), fTheta(0), fPhi(0), fCtx(0), fCty(0), fTanAlpha(0), fTanThetaSinPhi(0),
+        fTanThetaCosPhi(0)
+  {
     SetAlpha(alpha);
     SetThetaAndPhi(theta, phi);
-  }  
+  }
 
   VECGEOM_CUDA_HEADER_BOTH
-  void SetAlpha(const T alpha) {
+  void SetAlpha(const T alpha)
+  {
     fAlpha    = alpha;
     fTanAlpha = tan(kDegToRad * alpha);
     ComputeNormals();
   }
 
   VECGEOM_CUDA_HEADER_BOTH
-  void SetTheta(const T theta)
-  {
-    SetThetaAndPhi(theta, fPhi);
-  }
+  void SetTheta(const T theta) { SetThetaAndPhi(theta, fPhi); }
 
   VECGEOM_CUDA_HEADER_BOTH
-  void SetPhi(const T phi)
-  {
-    SetThetaAndPhi(fTheta, phi);
-  }
+  void SetPhi(const T phi) { SetThetaAndPhi(fTheta, phi); }
 
   VECGEOM_CUDA_HEADER_BOTH
   void SetThetaAndPhi(const T theta, const T phi)
@@ -78,8 +75,8 @@ struct ParallelepipedStruct {
   VECGEOM_CUDA_HEADER_BOTH
   void ComputeNormals()
   {
-    Vector3D<T> v(sin(kDegToRad * fTheta) * cos(kDegToRad * fPhi),
-                          sin(kDegToRad * fTheta) * sin(kDegToRad * fPhi), cos(kDegToRad * fTheta));
+    Vector3D<T> v(sin(kDegToRad * fTheta) * cos(kDegToRad * fPhi), sin(kDegToRad * fTheta) * sin(kDegToRad * fPhi),
+                  cos(kDegToRad * fTheta));
     Vector3D<T> vx(1., 0., 0.);
     Vector3D<T> vy(-sin(kDegToRad * fAlpha), -cos(kDegToRad * fAlpha), 0.);
     fNormals[0] = v.Cross(vy);
@@ -90,7 +87,6 @@ struct ParallelepipedStruct {
     fCtx = 1.0 / sqrt(1. + fTanAlpha * fTanAlpha + fTanThetaCosPhi * fTanThetaCosPhi);
     fCty = 1.0 / sqrt(1. + fTanThetaSinPhi * fTanThetaSinPhi);
   }
-
 };
 }
 } // end
