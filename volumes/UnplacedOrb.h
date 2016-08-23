6d1aef63ce30286d2cec2dff93a8214f5afcf9e5
Sandro Wenzel <sandro.wenzel@cern.ch>
2016-08-23 10:03:18 +0200

    [VECGEOM-35] Fix 'override' compiler warning

--- volumes/UnplacedOrb.h
+++ volumes/UnplacedOrb.h
@@ -54,7 +54,7 @@ public:
   void SetRadius(Precision r);
 
   VECGEOM_CUDA_HEADER_BOTH
-  void Extent(Vector3D<Precision> &, Vector3D<Precision> &) const;
+  void Extent(Vector3D<Precision> &, Vector3D<Precision> &) const override;
 
   VECGEOM_CUDA_HEADER_BOTH
   VECGEOM_FORCE_INLINE
@@ -64,7 +64,7 @@ public:
   VECGEOM_FORCE_INLINE
   Precision SurfaceArea() const { return fSurfaceArea; }
 
-  virtual Vector3D<Precision> GetPointOnSurface() const;
+  virtual Vector3D<Precision> GetPointOnSurface() const override;
 
   VECGEOM_CUDA_HEADER_BOTH
   virtual bool Normal(Vector3D<Precision> const &p, Vector3D<Precision> &normal) const override
