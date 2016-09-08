0158432ee83d1d318cc24db70c29a3f4587a8467
Guilherme Amadio <amadio@ncc.unesp.br>
2016-09-08 11:31:05 -0300

    Improvements to SphereUtilities
    - Remove commented out dead code
    - Implement functions as templates to work for both float/double
    - Make short functions inline

--- volumes/SphereUtilities.h
+++ volumes/SphereUtilities.h
@@ -5,48 +5,35 @@
 
 #ifndef VECGEOM_NVCC
 #include "base/RNG.h"
-//#include <iostream>
-//#include <fstream>
-//#include <limits>
-#include <cmath>
-////#include <cfloat>
-//#include <vector>
-//#include <algorithm>
-
 #endif
 
 namespace vecgeom {
 inline namespace VECGEOM_IMPL_NAMESPACE {
 
+template <typename T>
+VECGEOM_FORCE_INLINE
 VECGEOM_CUDA_HEADER_BOTH
-Precision sqr(Precision x)
+T sqr(T x)
 {
   return x * x;
-};
-
-/*
-template <class Backend>
-VECGEOM_CUDA_HEADER_BOTH
-void fabs(typename Backend::precision_v &v)
-{
-    typedef typename Backend::precision_v Double_t;
-    Double_t mone(-1.);
-    MaskedAssign( (v<0), mone*v , &v );
 }
-*/
-#ifndef VECGEOM_NVCC
-Precision GetRadiusInRing(Precision rmin, Precision rmax)
-{
 
-  // Generate radius in annular ring according to uniform area
-  //
-  if (rmin <= 0.) {
-    return rmax * std::sqrt(RNG::Instance().uniform(0., 1.));
-  }
-  if (rmin != rmax) {
-    return std::sqrt(RNG::Instance().uniform(0., 1.) * (sqr(rmax) - sqr(rmin)) + sqr(rmin));
-  }
-  return rmin;
+#ifndef VECGEOM_NVCC
+// Generate radius in annular ring according to uniform area
+template <typename T>
+VECGEOM_FORCE_INLINE
+T GetRadiusInRing(T rmin, T rmax)
+{
+  if (rmin == rmax) return rmin;
+
+  T rng(RNG::Instance().uniform(0.0, 1.0));
+
+  if (rmin <= T(0.0)) return rmax * Sqrt(rng);
+
+  T rmin2 = rmin * rmin;
+  T rmax2 = rmax * rmax;
+
+  return Sqrt(rng * (rmax2 - rmin2) + rmin2);
 }
 #endif
 }
