9185af7bc6838e53b957d5442f8148ae99a13ab8
Guilherme Lima <lima@fnal.gov>
2016-11-18 15:22:23 -0600

    [VECGEOM-393] Avoid double application of tan() to parameters

--- volumes/UnplacedTrapezoid.h
+++ volumes/UnplacedTrapezoid.h
@@ -68,8 +68,8 @@ public:
   /// \brief Fast constructor: all parameters from one array
   VECGEOM_CUDA_HEADER_BOTH
   UnplacedTrapezoid(Precision const *params)
-      : UnplacedTrapezoid(params[0], params[1], params[2], params[3], params[4], params[5], std::tan(params[6]),
-                          params[7], params[8], params[9], std::tan(params[10]))
+      : UnplacedTrapezoid(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7],
+                          params[8], params[9], params[10])
   {
   }
 
