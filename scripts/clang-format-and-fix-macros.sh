6d1aa58074dbf65875c8a94c8eb74f8fb805f2d0
Philippe Canal <pcanal@fnal.gov>
2017-03-31 14:54:52 -0500

    VECGEOM-317: Allow to output the result to stdout

--- scripts/clang-format-and-fix-macros.sh
+++ scripts/clang-format-and-fix-macros.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# If the first argument is just '-', do not replace the files
+# instead print the result to stdout.
+
 sed_i="-i .bak"
 testername=/tmp/sed_tester.$$.tmp
 touch $testername
@@ -71,44 +74,77 @@ s/\(\n[[:blank:]]*template[^;{(]*\)>[[:space:]]*${macro}/\1${short}>/g"
 s/ *${short}>\([[:space:]\n]*\)/>${newPattern}/g"
 done
 
+if [ "-" = "$1" ] ; then
+  shift;
+
+  for file in $@; do
+    cat $file | eval sed -n -e '"
+      # if the first line copy the pattern to the hold buffer
+      1h
+      # if not the first line then append the pattern to the hold buffer
+      1!H
+      # if the last line then ...
+      $ {
+        # copy from the hold to the pattern buffer
+        g
+        # do the search and replace
+        ${patternFrom}
+        # print
+        p
+      }"' | clang-format  | clang-format | eval sed -n -e '"
+      # if the first line copy the pattern to the hold buffer
+      1h
+      # if not the first line then append the pattern to the hold buffer
+      1!H
+      # if the last line then ...
+      $ {
+        # copy from the hold to the pattern buffer
+        g
+        # do the search and replace
+        ${patternTo}
+        # print
+        p
+      }"'
+  done
+else
 # Shorten the macro and move them out of the way so that they have
 # no effect on line length calculation.
-for file in $@; do
-  eval sed  ${sed_i} -n -e '"
-    # if the first line copy the pattern to the hold buffer
-    1h
-    # if not the first line then append the pattern to the hold buffer
-    1!H
-    # if the last line then ...
-    $ {
-      # copy from the hold to the pattern buffer
-      g
-      # do the search and replace
-      ${patternFrom}
-      # print
-      p
-    }"' '"${file}"'
-done
+  for file in $@; do
+    eval sed  ${sed_i} -n -e '"
+      # if the first line copy the pattern to the hold buffer
+      1h
+      # if not the first line then append the pattern to the hold buffer
+      1!H
+      # if the last line then ...
+      $ {
+        # copy from the hold to the pattern buffer
+        g
+        # do the search and replace
+        ${patternFrom}
+        # print
+        p
+      }"' '"${file}"'
+  done
 
-clang-format -i "$@"
-# Run clang-format a 2nd time, this stabilizes some of the comment positioning.
-clang-format -i "$@"
+  clang-format -i "$@"
+  # Run clang-format a 2nd time, this stabilizes some of the comment positioning.
+  clang-format -i "$@"
 
-# Put back the macros.
-for file in $@; do
-  eval sed   ${sed_i} -n -e '"
-    # if the first line copy the pattern to the hold buffer
-    1h
-    # if not the first line then append the pattern to the hold buffer
-    1!H
-    # if the last line then ...
-    $ {
-      # copy from the hold to the pattern buffer
-      g
-      # do the search and replace
-      ${patternTo}
-      # print
-      p
-    }"' '"${file}"'
-done
-
+  # Put back the macros.
+  for file in $@; do
+    eval sed   ${sed_i} -n -e '"
+      # if the first line copy the pattern to the hold buffer
+      1h
+      # if not the first line then append the pattern to the hold buffer
+      1!H
+      # if the last line then ...
+      $ {
+        # copy from the hold to the pattern buffer
+        g
+        # do the search and replace
+        ${patternTo}
+        # print
+        p
+      }"' '"${file}"'
+  done
+fi
