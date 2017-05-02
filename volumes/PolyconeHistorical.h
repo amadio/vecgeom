420f7be0846ad5ea39b70103f051fb60434f12df
Raman Sehgal <raman.sehgal@cern.ch>
2017-05-02 13:36:58 +0530

    [VECGEOM-256]; Applied Clang-formatting to volumes/PolyconeHistorical.h

--- volumes/PolyconeHistorical.h
+++ volumes/PolyconeHistorical.h
@@ -8,7 +8,6 @@
 #ifndef VOLUMES_POLYCONEHISTORICAL_H_
 #define VOLUMES_POLYCONEHISTORICAL_H_
 
-
 /*
 #include "base/Global.h"
 #include "base/AlignedBase.h"
@@ -22,77 +21,76 @@
 namespace vecgeom {
 inline namespace VECGEOM_IMPL_NAMESPACE {
 
-class PolyconeHistorical
-{
-  public:
-    PolyconeHistorical():fHStart_angle(0.), fHOpening_angle(0.), fHNum_z_planes(0),fHZ_values(0), fHRmin(0), fHRmax(0){}
-    PolyconeHistorical( int z_planes ):fHStart_angle(0.), fHOpening_angle(0.), fHNum_z_planes(z_planes){
-    	fHZ_values = new double[z_planes];
-    	fHRmin = new double[z_planes];
-    	fHRmax = new double[z_planes];
-    	for(int i=0; i<z_planes;i++){
-    		fHZ_values[i]=0.0;
-    		fHRmin[i]=0.0;
-    		fHRmax[i]=0.0;
-    	}
+class PolyconeHistorical {
+public:
+  PolyconeHistorical() : fHStart_angle(0.), fHOpening_angle(0.), fHNum_z_planes(0), fHZ_values(0), fHRmin(0), fHRmax(0)
+  {
+  }
+  PolyconeHistorical(int z_planes) : fHStart_angle(0.), fHOpening_angle(0.), fHNum_z_planes(z_planes)
+  {
+    fHZ_values = new double[z_planes];
+    fHRmin     = new double[z_planes];
+    fHRmax     = new double[z_planes];
+    for (int i = 0; i < z_planes; i++) {
+      fHZ_values[i] = 0.0;
+      fHRmin[i]     = 0.0;
+      fHRmax[i]     = 0.0;
     }
-    ~PolyconeHistorical(){
-    	delete [] fHZ_values;
-    	delete [] fHRmin;
-    	delete [] fHRmax;
+  }
+  ~PolyconeHistorical()
+  {
+    delete[] fHZ_values;
+    delete[] fHRmin;
+    delete[] fHRmax;
+  }
+  PolyconeHistorical(const PolyconeHistorical &source)
+  {
+    fHStart_angle   = source.fHStart_angle;
+    fHOpening_angle = source.fHOpening_angle;
+    fHNum_z_planes  = source.fHNum_z_planes;
+
+    fHZ_values = new double[fHNum_z_planes];
+    fHRmin     = new double[fHNum_z_planes];
+    fHRmax     = new double[fHNum_z_planes];
+
+    for (int i = 0; i < fHNum_z_planes; i++) {
+      fHZ_values[i] = source.fHZ_values[i];
+      fHRmin[i]     = source.fHRmin[i];
+      fHRmax[i]     = source.fHRmax[i];
     }
-    PolyconeHistorical( const PolyconeHistorical& source ){
-    	  fHStart_angle   = source.fHStart_angle;
-    	  fHOpening_angle = source.fHOpening_angle;
-    	  fHNum_z_planes  = source.fHNum_z_planes;
+  }
+  PolyconeHistorical &operator=(const PolyconeHistorical &right)
+  {
+    if (&right == this) return *this;
 
-    	  fHZ_values  = new double[fHNum_z_planes];
-    	  fHRmin      = new double[fHNum_z_planes];
-    	  fHRmax      = new double[fHNum_z_planes];
+    fHStart_angle   = right.fHStart_angle;
+    fHOpening_angle = right.fHOpening_angle;
+    fHNum_z_planes  = right.fHNum_z_planes;
 
-    	  for( int i = 0; i < fHNum_z_planes; i++)
-    	  {
-    		fHZ_values[i] = source.fHZ_values[i];
-    	    fHRmin[i]     = source.fHRmin[i];
-    	    fHRmax[i]     = source.fHRmax[i];
-    	  }
+    delete[] fHZ_values;
+    delete[] fHRmin;
+    delete[] fHRmax;
+    fHZ_values = new double[fHNum_z_planes];
+    fHRmin     = new double[fHNum_z_planes];
+    fHRmax     = new double[fHNum_z_planes];
 
-    }
-    PolyconeHistorical& operator=( const PolyconeHistorical& right ){
-    	  if ( &right == this ) return *this;
-
-    	  fHStart_angle   = right.fHStart_angle;
-    	  fHOpening_angle = right.fHOpening_angle;
-    	  fHNum_z_planes  = right.fHNum_z_planes;
-
-    	  delete [] fHZ_values;
-    	  delete [] fHRmin;
-    	  delete [] fHRmax;
-    	  fHZ_values  = new double[fHNum_z_planes];
-    	  fHRmin      = new double[fHNum_z_planes];
-    	  fHRmax      = new double[fHNum_z_planes];
-
-    	  for( int i = 0; i < fHNum_z_planes; i++)
-    	  {
-    		  fHZ_values[i] = right.fHZ_values[i];
-    	    fHRmin[i]     = right.fHRmin[i];
-    	    fHRmax[i]     = right.fHRmax[i];
-    	  }
-
-    	  return *this;
+    for (int i = 0; i < fHNum_z_planes; i++) {
+      fHZ_values[i] = right.fHZ_values[i];
+      fHRmin[i]     = right.fHRmin[i];
+      fHRmax[i]     = right.fHRmax[i];
     }
 
-    double fHStart_angle;
-    double fHOpening_angle;
-    int   fHNum_z_planes;
-    double *fHZ_values;
-    double *fHRmin;
-    double *fHRmax;
+    return *this;
+  }
+
+  double fHStart_angle;
+  double fHOpening_angle;
+  int fHNum_z_planes;
+  double *fHZ_values;
+  double *fHRmin;
+  double *fHRmax;
 };
-
-
 }
-
 }
 
 #endif /* VOLUMES_POLYCONEHISTORICAL_H_ */
