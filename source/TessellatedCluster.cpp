4e264f0479fc3c8e8b35e1020954338b2d5d0b71
Mihaela Gheata <Mihaela.Gheata@cern.ch>
2017-08-09 09:02:01 +0200

    [VECGEOM-415] Protection for UME::SIMD not implementing streaming of Real_v types

--- source/TessellatedCluster.cpp
+++ source/TessellatedCluster.cpp
@@ -5,6 +5,7 @@ inline namespace VECGEOM_IMPL_NAMESPACE {
 
 std::ostream &operator<<(std::ostream &os, TriangleFacet<double> const &facet)
 {
+#ifndef VECCORE_ENABLE_UMESIMD
   os << "  vertices: {" << facet.fVertices[0] << ", " << facet.fVertices[1] << ", " << facet.fVertices[2] << "}\n";
   os << "    indices:  {" << facet.fIndices << "}\n";
   if (facet.fNeighbors.size()) {
@@ -16,15 +17,18 @@ std::ostream &operator<<(std::ostream &os, TriangleFacet<double> const &facet)
   }
   os << "    normal: {" << facet.fNormal << "}\n";
   os << "    distance: " << facet.fDistance << "}";
+#endif
   return os;
 }
 
 std::ostream &operator<<(std::ostream &os, TessellatedCluster<typename vecgeom::VectorBackend::Real_v> const &tcl)
 {
+#ifndef VECCORE_ENABLE_UMESIMD
   os << "normals: {" << tcl.fNormals << "}\n";
   os << "distances: {" << tcl.fDistances << "}\n";
   os << "side vectors: {" << tcl.fSideVectors[0] << "}\n\t{" << tcl.fSideVectors[1] << "}\n\t{" << tcl.fSideVectors[2]
      << "}";
+#endif
   return os;
 }
 
