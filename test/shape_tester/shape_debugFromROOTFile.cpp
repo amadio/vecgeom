a1c7b5e6534c03e1f2fd6e4397d223e7d62c7f2b
Mihaela Gheata <Mihaela.Gheata@cern.ch>
2017-10-31 12:59:11 +0100

    [VECGEOM-453] Clang format

--- test/shape_tester/shape_debugFromROOTFile.cpp
+++ test/shape_tester/shape_debugFromROOTFile.cpp
@@ -23,26 +23,25 @@ using namespace vecgeom;
 //   option : [--usolids(default)|--vecgeom]
 // logicalvolumename should not contain trailing pointer information
 
-
 double runTester(VPlacedVolume const *shape, Vector3D<double> const &point, Vector3D<double> const &dir)
 {
   const char *sInside[3] = {"kInside", "kSurface", "kOutside"};
   double distance;
   auto inside = shape->Inside(point);
-  std::cout << "Inside = " << sInside[inside-1] << std::endl;
-  distance = shape->SafetyToIn(point);
+  std::cout << "Inside = " << sInside[inside - 1] << std::endl;
+  distance                                = shape->SafetyToIn(point);
   if (distance < kHalfTolerance) distance = 0.0;
   std::cout << "SafetyFromOutside = " << distance << std::endl;
-  distance = shape->SafetyToOut(point);
+  distance                                = shape->SafetyToOut(point);
   if (distance < kHalfTolerance) distance = 0.0;
   std::cout << "SafetyFromInside = " << distance << std::endl;
-  distance = shape->DistanceToIn(point, dir);
+  distance                                = shape->DistanceToIn(point, dir);
   if (distance < kHalfTolerance) distance = 0.0;
   std::cout << "DistanceToIn = " << distance << std::endl;
-  distance = shape->DistanceToOut(point, dir);
+  distance                                = shape->DistanceToOut(point, dir);
   if (distance < kHalfTolerance) distance = 0.0;
   std::cout << "DistanceToOut = " << distance << std::endl;
-  
+
   if (shape) delete shape;
   return distance;
 }
@@ -52,12 +51,12 @@ void DrawArrow(Vector3D<double> const &point, Vector3D<double> const &dir, doubl
   TPolyLine3D *pl = new TPolyLine3D(2);
   pl->SetLineColor(color);
   pl->SetNextPoint(point[0], point[1], point[2]);
-  pl->SetNextPoint(point[0]+size*dir[0], point[1]+size*dir[1], point[2]+size*dir[2]);
+  pl->SetNextPoint(point[0] + size * dir[0], point[1] + size * dir[1], point[2] + size * dir[2]);
   TPolyMarker3D *pm1 = new TPolyMarker3D(2);
   TPolyMarker3D *pm2 = new TPolyMarker3D(1);
   pm1->SetNextPoint(point[0], point[1], point[2]);
-  pm1->SetNextPoint(point[0]+dout*dir[0], point[1]+dout*dir[1], point[2]+dout*dir[2]);
-  pm2->SetNextPoint(point[0]+size*dir[0], point[1]+size*dir[1], point[2]+size*dir[2]);
+  pm1->SetNextPoint(point[0] + dout * dir[0], point[1] + dout * dir[1], point[2] + dout * dir[2]);
+  pm2->SetNextPoint(point[0] + size * dir[0], point[1] + size * dir[1], point[2] + size * dir[2]);
   pm1->SetMarkerColor(2);
   pm2->SetMarkerColor(color);
   pm1->SetMarkerStyle(6);
@@ -70,10 +69,10 @@ void DrawArrow(Vector3D<double> const &point, Vector3D<double> const &dir, doubl
 
 TGeoNode *FindDaughter(TGeoVolume *vol, std::string &name, int &index)
 {
-  int nd = vol->GetNdaughters();
+  int nd          = vol->GetNdaughters();
   TGeoNode *found = nullptr;
-  for (int i=0; i<nd; ++i) {
-    TGeoNode *node = vol->GetNode(i);  
+  for (int i = 0; i < nd; ++i) {
+    TGeoNode *node = vol->GetNode(i);
     if (name == node->GetVolume()->GetName()) {
       found = node;
       index = i;
@@ -84,15 +83,12 @@ TGeoNode *FindDaughter(TGeoVolume *vol, std::string &name, int &index)
     }
   }
   if (found) {
-    for (int i=0; i<nd; ++i) {
-      if (vol->GetNode(i) != found)
-        vol->GetNode(i)->GetVolume()->SetVisibility(false);
+    for (int i = 0; i < nd; ++i) {
+      if (vol->GetNode(i) != found) vol->GetNode(i)->GetVolume()->SetVisibility(false);
     }
   }
   return found;
 }
-      
-   
 
 int main(int argc, char *argv[])
 {
@@ -109,26 +105,23 @@ int main(int argc, char *argv[])
   std::string testdaughter(argv[3]);
   // Local point/direction (mm)
   double point[3];
-  point[0] = 0.1*atof(argv[4]);
-  point[1] = 0.1*atof(argv[5]);
-  point[2] = 0.1*atof(argv[6]);
+  point[0] = 0.1 * atof(argv[4]);
+  point[1] = 0.1 * atof(argv[5]);
+  point[2] = 0.1 * atof(argv[6]);
   double lpoint[3];
-  memcpy(lpoint, point, 3*sizeof(double));
+  memcpy(lpoint, point, 3 * sizeof(double));
   double direction[3];
   direction[0] = atof(argv[7]);
   direction[1] = atof(argv[8]);
   direction[2] = atof(argv[9]);
   double ldir[3];
-  memcpy(ldir, direction, 3*sizeof(double));
-  
+  memcpy(ldir, direction, 3 * sizeof(double));
 
-  bool daughter = false;
-  if (testdaughter != "void")
-    daughter = true;
-    
-    
-  int found               = 0;
-  TGeoVolume *foundvolume = NULL;
+  bool daughter                        = false;
+  if (testdaughter != "void") daughter = true;
+
+  int found                 = 0;
+  TGeoVolume *foundvolume   = NULL;
   TGeoVolume *founddaughter = NULL;
   // now try to find shape with logical volume name given on the command line
   TObjArray *vlist = gGeoManager->GetListOfVolumes();
@@ -159,50 +152,52 @@ int main(int argc, char *argv[])
       return 1;
     }
     founddaughter = dnode->GetVolume();
-    
+
     std::cerr << "daughter found\n" << std::endl;
     founddaughter->GetShape()->InspectShape();
     dnode->GetMatrix()->MasterToLocal(point, lpoint);
     dnode->GetMatrix()->MasterToLocalVect(direction, ldir);
   }
 
-/*
-  double master[3] = {-70.37002915950117,-193.7782403798211,-382.7815168187679};
-  double masterdir[3] = {0.9910804931325202, 0.06730730580739153, 0.1150181842890534};
-  TGeoNode *node = gGeoManager->FindNode(master[0], master[1], master[2]);
-  if (node && (node->GetVolume() == foundvolume)) {
-    std::cout << " == Path is correct\n";
-    if (daughter) gGeoManager->CdDown(index);
-    std::cout << "touchable: " << gGeoManager->GetPath() << std::endl;  
-    gGeoManager->GetCurrentMatrix()->MasterToLocal(master, lpoint);
-    gGeoManager->GetCurrentMatrix()->MasterToLocalVect(masterdir, ldir);
-  }
-*/
-  
-  LogicalVolume *converted = (daughter) ? RootGeoManager::Instance().Convert(founddaughter)
-                                        : RootGeoManager::Instance().Convert(foundvolume);
+  /*
+    double master[3] = {-70.37002915950117,-193.7782403798211,-382.7815168187679};
+    double masterdir[3] = {0.9910804931325202, 0.06730730580739153, 0.1150181842890534};
+    TGeoNode *node = gGeoManager->FindNode(master[0], master[1], master[2]);
+    if (node && (node->GetVolume() == foundvolume)) {
+      std::cout << " == Path is correct\n";
+      if (daughter) gGeoManager->CdDown(index);
+      std::cout << "touchable: " << gGeoManager->GetPath() << std::endl;
+      gGeoManager->GetCurrentMatrix()->MasterToLocal(master, lpoint);
+      gGeoManager->GetCurrentMatrix()->MasterToLocalVect(masterdir, ldir);
+    }
+  */
+
+  LogicalVolume *converted =
+      (daughter) ? RootGeoManager::Instance().Convert(founddaughter) : RootGeoManager::Instance().Convert(foundvolume);
 
   VPlacedVolume *shape = converted->Place(); // VPlacedVolume
   std::cerr << "\n=========Using VecGeom=========\n\n";
   shape->Print();
   Vector3D<double> amin, amax;
   shape->Extent(amin, amax);
-  double size = 0.2*(amax - amin).Mag();
-  
+  double size = 0.2 * (amax - amin).Mag();
+
   Vector3D<double> lp(lpoint[0], lpoint[1], lpoint[2]);
   Vector3D<double> ld(ldir[0], ldir[1], ldir[2]);
   printf("local point: %.16f %.16f %.16f  local dir: %.16f %.16f %.16f\n", lp[0], lp[1], lp[2], ld[0], ld[1], ld[2]);
-    
-  double dout = runTester(shape, lp, ld);
+
+  double dout       = runTester(shape, lp, ld);
   TApplication *app = new TApplication("VecGeom Visualizer", nullptr, nullptr);
-  TCanvas * c = new TCanvas(foundvolume->GetName(), "", 1200, 800);
+  TCanvas *c        = new TCanvas(foundvolume->GetName(), "", 1200, 800);
   gGeoManager->SetTopVisible();
   gGeoManager->SetVisLevel(1);
   foundvolume->SetTransparency(40);
   foundvolume->SetVisContainers();
   foundvolume->SetLineColor(kBlue);
-  if (!daughter) foundvolume->DrawOnly();
-  else foundvolume->Draw();
+  if (!daughter)
+    foundvolume->DrawOnly();
+  else
+    foundvolume->Draw();
   DrawArrow(lp, ld, size, dout, kMagenta);
   c->GetView()->ShowAxis();
   app->Run();
