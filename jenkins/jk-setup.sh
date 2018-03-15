63092f4ac4ca0b5561d077c34dcd74e088695449
Pere Mato <pere.mato@cern.ch>
2018-03-15 11:16:25 +0100

    Add cache environment

--- jenkins/jk-setup.sh
+++ jenkins/jk-setup.sh
@@ -93,6 +93,12 @@ elif [[ $COMPILER == *clang* ]]; then
   export FC=`which gfortran`
 fi
 
+if [[ $LABEL == slc6 || $LABEL == centos7 ]]; then
+    export CCACHE_BASEDIR=/mnt/build/jenkins/workspace/
+    export CCACHE_DIR=/ccache
+    export CCACHE_MAXSIZE=10G
+fi
+
 export CMAKE_SOURCE_DIR=$WORKSPACE/VecGeom
 export CMAKE_BINARY_DIR=$WORKSPACE/build
 export CMAKE_INSTALL_PREFIX=$WORKSPACE/install
