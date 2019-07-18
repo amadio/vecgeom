8e201846d2c878fa326072ecc714c041e5e33786
Murat Topak <murat.topak@cern.ch>
2019-07-18 15:06:51 +0000

    Remove unneeded call to clear()

--- source/UnplacedEllipticalCone.cpp
+++ source/UnplacedEllipticalCone.cpp
@@ -231,7 +231,6 @@ SolidMesh *UnplacedEllipticalCone::CreateMesh3D(Transformation3D const &trans, c
 
     Utils3D::vector_t<size_t> indices;
     indices.reserve(nMeshVertices / 2);
-    indices.clear();
 
     // upper surface
     for (size_t i = 0; i < nMeshVertices; i += 2) {
